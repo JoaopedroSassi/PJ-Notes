e1fe5505506b9b9d06dd804f2d9028e800dd85b9
Sassi <joaopedro10813@gmail.com>
2022-01-26 13:40:19 -0300

    Add handlebars + css

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8000;
 
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
 //Routes
 app.get('/', (req, res) => {
-   res.send("App ta funcionando!");
+   res.render('home');
 });
 
 app.listen(port, () => {
