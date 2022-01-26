4e440419d6ee4edf457c11105a09100a9e8ea11b
Sassi <joaopedro10813@gmail.com>
2022-01-26 14:47:32 -0300

    View notes in home

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 const notesRoutes = require('./routes/notes');
 
 //Routes
-app.get('/', (req, res) => {
-   res.render('home');
+app.get('/', async function(req, res) {
+   const notes = await db.getDb().db().collection('notes').find({}).toArray();
+   res.render('home', {notes});
 });
 
 app.use('/notes', notesRoutes);
