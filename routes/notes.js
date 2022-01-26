6afd2a86da980b9f095c042a25405d2c5140dc43
Sassi <joaopedro10813@gmail.com>
2022-01-26 14:33:18 -0300

    Add insert function

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,24 @@
 const Router = require('express').Router;
+const db = require('../db/connection');
+const {ObjectId} = require('mongodb');
+
 const router = Router();
 
 router.get('/', (req, res) => {
    res.render('notes/create');
+});
+
+router.post('/', (req, res) => {
+   const data = req.body;
+   const title = data.title;
+   const description = data.description;
+
+   db.getDb()
+     .db()
+     .collection('notes')
+     .insertOne({title: title, description: description});
+
+   res.redirect(301, '/');
 });
 
 module.exports = router;
